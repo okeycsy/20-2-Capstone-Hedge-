11920b0a2e27a500dc9a5f63cd85f74aec7d830d
nalgaeg1t <49203563+nalgaeg1t@users.noreply.github.com>
2020-11-25 23:46:07 +0900

    t
    t

---
+++ react/src/src_2/Stock_Details.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
+
+const stock_data = {
+  yesterday_close:37300,
+  high:377500,
+  low:36600,
+  today_close:37750,
+  volume: 700638,
+}
+
+export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  
+  useEffect(() => {
+    setData(stock_data)
+    setLoading(false)
+  }, []);
+
+
+  return (
+    <View>
+      {isLoading ? <ActivityIndicator/> : (
+        <View>
+          <View style={styles.row}><Text>전날 종가</Text><Text>{data.yesterday_close}</Text></View>
+          <View style={styles.row}><Text>당일 종가</Text><Text>{data.today_close}</Text></View>
+          <View style={styles.row}><Text>당일 고가</Text><Text>{data.high}</Text></View>
+          <View style={styles.row}><Text>당일 저가</Text><Text>{data.low}</Text></View>
+          <View style={styles.row}><Text>거래량</Text><Text>{data.volume}</Text></View>
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  row: {
+    flex: 1,
+    marginBottom: 5,
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  }
+});
