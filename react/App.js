09d2d0039a5cfcef4ca55e4b4dfd03297ba54b2c
Durumi <DrummyCrane@users.noreply.github.com>
2020-11-26 00:59:19 +0900

    Install react-chartjs-2
    Install react-chartjs-2
    add charts

--- react/App.js
+++ react/App.js
@@ -11,7 +11,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from "./src/Home";
-import DetailsScreen from "./src/Dividend";
+import DetailsScreen from "./src/ProductChart";
 import StockScreen from "./src/src_2/Details";
 import Stock_Details from "./src/src_2/Stock_Details";
 
