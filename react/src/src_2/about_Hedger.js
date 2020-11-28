34944b6edcaac2e78c8b715ee6825b53fd6aad24
nalgaeg1t <49203563+nalgaeg1t@users.noreply.github.com>
2020-11-28 21:59:05 +0900

    애니메이션
    멋지게 만들어봐요

--- react/src/src_2/about_Hedger.js
+++ react/src/src_2/about_Hedger.js
@@ -2,31 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Image, ScrollView , Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
+import Test from './Test'
+
 export default function about_Hedger() {
   return (
     <ScrollView
         contentContainerStyle={styles.contentContainer}
-    >
-        <View style={styles.subContainer}>
-            <Image
-            style={{ width: 600, height: 300}}
-            source={require('../../image/소개_1.png')}
-            />
-        </View>
-
-        <View style={styles.subContainer}>
-            <Image
-            style={{ width: 600, height: 300}}
-            source={require('../../image/소개_2.png')}
-            />
-        </View>
-
-        <View style={styles.subContainer}>
-            <Image
-            style={{ width: 600, height: 300}}
-            source={require('../../image/소개_3.png')}
-            />
-        </View>
+    >        
+        <Test/>
     </ScrollView>
   );
 }
